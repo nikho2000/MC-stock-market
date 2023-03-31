58268d0d717e03429658a02ce7285ecd3e78d409
Niklas <niklas-holl@gmx.de>
2023-03-31 12:15:44 +0200

    initial commit

---
+++ src/main/java/de/nikho2000/mcstockmarket/MC_stock_market.java
@@ -0,0 +1,17 @@
+package de.nikho2000.mcstockmarket;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MC_stock_market extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
